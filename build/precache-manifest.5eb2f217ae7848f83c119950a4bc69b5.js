0f7e73d379932c387aaff332c197b95282f3b1b5
Maria <murikmarusia@gmail.com>
2019-03-01 00:33:06 +0200

    deploy 1

---
+++ build/precache-manifest.5eb2f217ae7848f83c119950a4bc69b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e14bcd86ffa7844e2c8f",
+    "url": "/todo-app_react/build/static/js/runtime~main.e14bcd86.js"
+  },
+  {
+    "revision": "cb6f30f3917a298355e0",
+    "url": "/todo-app_react/build/static/js/main.cb6f30f3.chunk.js"
+  },
+  {
+    "revision": "227790b2be4741aac0c1",
+    "url": "/todo-app_react/build/static/js/2.227790b2.chunk.js"
+  },
+  {
+    "revision": "cb6f30f3917a298355e0",
+    "url": "/todo-app_react/build/static/css/main.a448c6dc.chunk.css"
+  },
+  {
+    "revision": "87ac3fd74f5110c558ab362deef6c225",
+    "url": "/todo-app_react/build/index.html"
+  }
+];
